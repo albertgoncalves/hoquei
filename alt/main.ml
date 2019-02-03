c91834550097d8780d3542e8e0b15d5142671f61
albertgoncalves <goncalves.albert@gmail.com>
2019-02-03 15:54:11 -0500

    Didn't get much faster.

--- alt/main.ml
+++ alt/main.ml
@@ -24,26 +24,23 @@ let scalpel (l : string) : string list =
             loop target accu (ys, xs) in
     loop false [] ([], E.explode l)
 
-let rows (l : string) : string option =
+let rows : (string list -> string list list) =
     let pattern = Str.regexp "<tr .*" in
-    if Str.string_match pattern l 0 then
-        Some l
-    else
-        None
-
-let sift (l : 'a option list) : 'a list =
-    let rec loop (accu : 'a list) : ('a option list -> 'a list) = function
+    let rec loop (accu : string list list)
+        : (string list -> string list list) = function
         | [] -> accu
-        | (Some x)::xs -> loop (x::accu) xs
-        | None::xs -> loop accu xs in
-    loop [] l
+        | x::xs ->
+            if Str.string_match pattern x 0 then
+                loop ((scalpel x)::accu) xs
+            else
+                loop accu xs in
+    loop []
 
 let html : string list = U.with_file Sys.argv.(1) Std.input_list
 
 let main () : unit =
-    L.map rows html
-    |> sift
-    |> L.map scalpel
+    html
+    |> rows
     |> L.iter (print_endline @. S.concat " ")
 
 let () = main ()
