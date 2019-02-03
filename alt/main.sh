bc8693df5283349f14bb9d1d0d1aa670aaa9e9a9
albertgoncalves <goncalves.albert@gmail.com>
2019-02-03 14:53:07 -0500

    Small adjustment.

--- alt/main.sh
+++ alt/main.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-fn="tmp.html"
+ocamlfind ocamlopt -package extlib,str -linkpkg main.ml -o main
+
+fn="index.html"
 if [ ! -e $fn ]; then
     curl https://www.hockey-reference.com/leagues/NHL_2019_games.html > $fn
 fi
 
-ocamlfind ocamlopt -package str,extlib -linkpkg main.ml -o main
-./main tmp.html
+./main $fn
