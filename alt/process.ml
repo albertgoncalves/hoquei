5903125b8d8254e3b74ab205f170b7354c427d38
albertgoncalves <goncalves.albert@gmail.com>
2019-02-03 23:59:04 -0500

    Need to build actual testing unit.

--- alt/process.ml
+++ alt/process.ml
@@ -8,11 +8,22 @@ type past_game =
     ; home_goals : int
     }
 
+type past_game_ot =
+    { date : string
+    ; away : string
+    ; home : string
+    ; away_goals : int
+    ; home_goals : int
+    ; ot : string
+    }
+
 type future_game =
     { date : string
     ; away : string
     ; home : string
     }
+
+type split_games = past_game list * past_game_ot list * future_game list
 
 exception InputValue of string
 
@@ -23,13 +34,12 @@ let string_to_goal (goals : string) (date : string) : int =
         let (err : string) = P.sprintf template goals date in
         raise (InputValue err)
 
-let split : (string list list -> (past_game list * future_game list)) =
-    let rec loop (past : past_game list) (future : future_game list)
-        : (string list list -> (past_game list * future_game list)) = function
-        | [h; a; d]::xs ->
-            let (x : future_game) =
-                {date = d; away = a; home = h} in
-            loop past (x::future) xs
+let split : (string list list -> split_games) =
+    let rec loop
+            (past : past_game list)
+            (past_ot : past_game_ot list)
+            (future : future_game list)
+        : (string list list -> split_games) = function
         | [_; _; hg; h; ag; a; d]::xs ->
             let (x : past_game) =
                 { date = d
@@ -38,10 +48,27 @@ let split : (string list list -> (past_game list * future_game list)) =
                 ; away_goals = string_to_goal ag d
                 ; home_goals = string_to_goal hg d
                 } in
-            loop (x::past) future xs
-        | _::xs -> loop past future xs
-        | [] -> (past, future) in
-    loop [] []
+            loop (x::past) past_ot future xs
+        | [_; _; o; hg; h; ag; a; d]::xs ->
+            let (x : past_game_ot) =
+                { date = d
+                ; away = a
+                ; home = h
+                ; away_goals = string_to_goal ag d
+                ; home_goals = string_to_goal hg d
+                ; ot = o
+                } in
+            loop past (x::past_ot) future xs
+        | [h; a; d]::xs ->
+            let (x : future_game) =
+                { date = d
+                ; away = a
+                ; home = h
+                } in
+            loop past past_ot (x::future) xs
+        | _::xs -> loop past past_ot future xs
+        | [] -> (past, past_ot, future) in
+    loop [] [] []
 
 let test_future =
     [ "Carolina Hurricanes"
@@ -59,5 +86,16 @@ let test_past =
     ; "2019-02-02"
     ]
 
-let test () : (past_game list * future_game list) =
-    split [[]; test_past; test_future]
+let test_past_ot =
+    [ "2:30"
+    ; "17,015"
+    ; "SO"
+    ; "2"
+    ; "Anaheim Ducks"
+    ; "3"
+    ; "Arizona Coyotes"
+    ; "2018-10-10"
+    ]
+
+let test () : split_games =
+    split [[]; test_past; test_past_ot; test_future]
