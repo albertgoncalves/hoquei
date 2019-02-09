48b95f339675feaa63490526858508d9cda4bddd
albertgoncalves <goncalves.albert@gmail.com>
2019-02-09 12:58:33 -0500

    Haskell version.

---
+++ alt/hs/shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; mkShell {
+    name = "Haskell";
+    buildInputs = [ (haskell.packages.ghc843.ghcWithPackages (pkgs: [
+
+                    ]))
+                    haskellPackages.hlint
+                    haskellPackages.hindent
+                    libiconv
+                  ];
+    shellHook = ''
+        if [ $(uname -s) = "Darwin" ]; then
+            alias ls='ls --color=auto'
+            alias ll='ls -al'
+        fi
+
+        alias hlint=hlint -c=never
+        alias hindent="hindent --indent-size 4 --sort-imports --line-length 79"
+    '';
+}
