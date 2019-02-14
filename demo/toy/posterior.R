8e0515ce158fdffc09f7e17865a2004f55fb2cd3
albertgoncalves <goncalves.albert@gmail.com>
2019-02-14 09:16:36 -0500

    A few things.

--- demo/toy/posterior.R
+++ demo/toy/posterior.R
@@ -2,10 +2,25 @@
 
 source("../utils.R")
 
-generate_sims = function(n, m, b, sigma) {
+sim_points = function(n, output_data) {
+    m_post = output_data$m
+    b_post = output_data$b
+    sigma_post = output_data$sigma
+
+    rows = sample(1:NROW(output_data), n, replace=TRUE)
     x = seq(-100, 1200, length.out=n)
-    y = rnorm(n, mean=(m * x) + b, sd=sigma)
-    return(list(x, y))
+    y = rnorm(n, mean=(m_post[rows] * x) + b_post[rows], sd=sigma_post[rows])
+
+    return(list(n, x, y))
+}
+
+sim_lines = function(n, output_data) {
+    m_post = output_data$m
+    b_post = output_data$b
+
+    rows = sample(1:NROW(output_data), n, replace=TRUE)
+
+    return(list(n, m_post[rows], b_post[rows]))
 }
 
 plot_curve = function(posterior_samples, init_value, title) {
@@ -13,28 +28,37 @@ plot_curve = function(posterior_samples, init_value, title) {
     abline(v=init_value, col="red")
 }
 
-plot_parameters = function(m, b, sigma, m_post, b_post, sigma_post) {
-    plot_curve(m_post, m, "m")
-    plot_curve(b_post, b, "b")
-    plot_curve(sigma_post, sigma, "sigma")
+plot_parameters = function(m, b, sigma, output_data) {
+    plot_curve(output_data$m, m, "m")
+    plot_curve(output_data$b, b, "b")
+    plot_curve(output_data$sigma, sigma, "sigma")
 }
 
-plot_model = function( x, y, m, b, sigma, ns, xs, ys, m_post, b_post
-                     , sigma_post) {
+plot_lines = function(n, m, b, alpha) {
+    for (i in 1:n) {
+        abline(a=b[i], b=m[i], col=adjustcolor("black", alpha))
+    }
+}
+
+plot_model = function(x, y, m, b, sigma, output_data, n_lines, n_points) {
     layout(matrix(c(1, 1, 1, 1, 1, 1, 2, 3, 4), 3, 3, byrow=TRUE))
 
-    plot(x, y, col="white")
-    points(xs, ys, col=adjustcolor("gray12", 750 / ns), pch=16)
+    sims = sim_points(n_points, output_data)
+    points_alpha = 750 / sims[[1]]
+    points_color = adjustcolor("gray12", points_alpha)
 
-    for (i in samples) {
-        abline(a=b_post[i], b=m_post[i], col=adjustcolor("black", 0.055))
-    }
+    lines = sim_lines(n_lines, output_data)
+    lines_alpha = 10 / lines[[1]]
+
+    plot(x, y, col="white")
+    points(sims[[2]], sims[[3]], col=points_color, pch=16)
+    plot_lines(lines[[1]], lines[[2]], lines[[3]], lines_alpha)
 
     abline(a=b, b=m, col="red")
     points(x, y, col=adjustcolor("coral", 0.85), pch=16)
     points(x, y, col=adjustcolor("white", 0.85))
 
-    plot_parameters(m, b, sigma, m_post, b_post, sigma_post)
+    plot_parameters(m, b, sigma, output_data)
 }
 
 plot_output = function(output_data) {
@@ -59,19 +83,6 @@ if (sys.nframe() == 0) {
     source("parameters.data.R")
     output_data = read_csv("output.csv")
 
-    n = 100
-    samples = sample(1:NROW(output_data), n, replace=TRUE)
-
-    m_post = output_data$m
-    b_post = output_data$b
-    sigma_post = output_data$sigma
-
-    ns = 10000
-    s = sample(1:NROW(output_data), ns, replace=TRUE)
-    sims = generate_sims(ns, m_post[s], b_post[s], sigma_post[s])
-    xs = sims[[1]]
-    ys = sims[[2]]
-
-    plot_model(x, y, m, b, sigma, ns, xs, ys, m_post, b_post, sigma_post)
+    plot_model(x, y, m, b, sigma, output_data, 1000, 10000)
     plot_output(output_data)
 }
