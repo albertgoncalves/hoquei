035725df48b1020b0d9e6b8127d5033b9d71d2ba
albertgoncalves <goncalves.albert@gmail.com>
2019-03-02 11:16:30 -0500

    This is nicer.

--- pbp/shifts.ml
+++ pbp/shifts.ml
@@ -4,7 +4,7 @@ module S = String
 module Y = Yojson.Basic
 module U = Y.Util
 
-exception InputValue of string
+exception Value of string
 
 let clock_to_seconds : (string option -> int) =
     let convert (minutes : string) (seconds : string) : int =
@@ -17,40 +17,31 @@ let clock_to_seconds : (string option -> int) =
             match S.split_on_char ':' time with
                 | [minutes; seconds] -> convert minutes seconds
                 | _ ->
-                    let error = P.sprintf "unable to convert '%s'" time in
-                    raise (InputValue error)
+                    let error = P.sprintf "unable to parse '%s'" time in
+                    raise (Value error)
 
 let rec last : ('a list -> 'a) = function
-    | [] -> raise (InputValue "empty list")
+    | [] -> raise (Value "empty list")
     | [x] -> x
     | (_::xs) -> last xs
-
-let demo (json : Y.json) (field : string) : int =
-    json
-    |> U.member field
-    |> U.to_string_option
-    |> clock_to_seconds
 
 let main () =
     let filename = "shifts.json" in
     let json = Y.from_file filename in
-    let data =
-        json
-        |> U.member "data"
-        |> U.to_list
-        |> last in
-
-    data
-    |> Y.pretty_to_string
-    |> print_endline;
-
-    L.iter
-        begin
-            fun field ->
-                demo data field
-                |> string_of_int
-                |> print_endline
-        end
-        ["startTime"; "duration"; "endTime"]
+    let data = json |> U.member "data" |> U.to_list |> last in
+    let print_blob () : unit = data |> Y.pretty_to_string |> print_endline in
+    let print_clocks () : unit =
+        L.iter
+            begin
+                fun field ->
+                    data
+                    |> U.member field
+                    |> U.to_string_option
+                    |> clock_to_seconds
+                    |> string_of_int
+                    |> print_endline
+            end
+            ["startTime"; "duration"; "endTime"] in
+    L.iter (fun f -> f ()) [print_blob; print_clocks]
 
 let () = main ()
