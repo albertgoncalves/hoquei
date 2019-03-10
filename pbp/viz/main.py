488731b5cdcae94e395b22ab783aeaf31af37b7b
albertgoncalves <goncalves.albert@gmail.com>
2019-03-09 23:28:57 -0500

    big viz progress

--- pbp/viz/main.py
+++ pbp/viz/main.py
@@ -32,8 +32,8 @@ def filter_shots(events):
     return events.loc[events.prev_result != "Penalty"].copy()
 
 
-def combine(game):
-    return lambda events: events.merge(game, how="left").copy()
+def combine(b):
+    return lambda a: a.merge(b, how="left").copy()
 
 
 def flip(events):
@@ -48,10 +48,11 @@ def flip(events):
 
 
 def colors(events):
+    # https://www.color-hex.com/color-palette/74832
     cmap = \
-        { "Missed Shot": 1.0
-        , "Shot": 0.5
-        , "Goal": 0.0
+        { "Missed Shot": "#ffd100"
+        , "Shot": "#58a291"
+        , "Goal": "#2c2d65"
         }
     events["color"] = events.result.map(cmap)
     return events.copy()
@@ -91,18 +92,6 @@ def rink(ax, goal):
         for xy in xys:
             ax.add_patch(patches.Circle(xy, 15, fill=None, alpha=alpha))
 
-    ax.set_aspect("equal")
-    ax.set_xlim([-100, 100])
-    ax.set_ylim([goal["edge"] * -1, goal["edge"]])
-
-    alpha = 0.2
-    for home in [True, False]:
-        draw_goal(goal, alpha=alpha, home=home)
-    draw_lines(alpha=alpha)
-    draw_circles(alpha=alpha)
-
-
-def plot(events):
     goal = \
         { "length": 2
         , "width": 6
@@ -110,23 +99,103 @@ def plot(events):
         , "y": -3
         , "edge": 45
         }
+    alpha = 0.2
 
-    _, ax = plt.subplots(figsize=(10, 5))
-    ax.scatter(events.x, events.y, c=events.color.values)
-    rink(ax, goal)
+    ax.set_aspect("equal")
+    ax.set_xlim([-100, 100])
+    ax.set_ylim([goal["edge"] * -1, goal["edge"]])
+
+    for home in [True, False]:
+        draw_goal(goal, alpha=alpha, home=home)
+
+    draw_lines(alpha=alpha)
+    draw_circles(alpha=alpha)
+
+
+def plot(events):
+    _, axs = plt.subplots(3, 1, figsize=(10, 15))
+    for i in range(3):
+        period = i + 1
+        subset = events.loc[events.period == period].copy()
+        axs[i].scatter(subset.x, subset.y, c=subset.color.values)
+        axs[i].set_title(period)
+        rink(axs[i])
     plt.tight_layout()
     plt.savefig("shots.png")
     plt.close()
 
 
+def goalie(shifts):
+    shifts["goalie"] = 0
+    shifts.loc[shifts.position == "G", "goalie"] = 1
+    return shifts.copy()
+
+
+def process(shifts):
+    columns = \
+        [ "game_id"
+        , "team_id"
+        , "venue"
+        , "team_name"
+        , "period"
+        , "second"
+        ]
+    params = {"player_id": "nunique", "goalie": "sum"}
+    shifts = shifts.groupby(by=columns, as_index=False).agg(params)
+    shifts.rename(columns={"player_id": "skaters"}, inplace=True)
+    shifts.skaters -= shifts.goalie
+    return shifts
+
+
+def pivot(shifts):
+    def slice(shifts, columns, venue):
+        subset = shifts.loc[shifts.venue == venue].copy()
+        params = \
+            { "skaters": "{}_skaters".format(venue)
+            , "goalie": "{}_goalie".format(venue)
+            }
+        subset.rename(columns=params, inplace=True)
+        return subset[columns + list(params.values())].copy()
+
+    columns = ["game_id", "period", "second"]
+    home = slice(shifts, columns, "home")
+    away = slice(shifts, columns, "away")
+    return home.merge(away, on=columns, how="outer").copy()
+
+
+def team_strength(events, special_teams=False):
+    rows = (
+        (events.home_skaters != 5)
+        | (events.home_goalie != 1)
+        | (events.away_skaters != 5)
+        | (events.away_goalie != 1)
+    )
+    if special_teams:
+        return events.loc[rows].copy()
+    else:
+        return events.loc[~rows].copy()
+
+
 def main():
-    pipe( read_csv("events.csv")
-        , combine(read_csv("game.csv"))
-        , filter_shots
-        , flip
-        , colors
-        , plot
-        )
+    game = read_csv("game.csv")
+    shifts = \
+        pipe( read_csv("shifts.csv")
+            , combine(game)
+            , combine(read_csv("players.csv"))
+            , goalie
+            , process
+            , pivot
+            )
+    events = \
+        pipe( read_csv("events.csv")
+            , combine(game)
+            , combine(shifts)
+            , filter_shots
+            , lambda events: team_strength(events, special_teams=False)
+            , flip
+            , colors
+            )
+    plot(events)
 
 
 if __name__ == "__main__":
